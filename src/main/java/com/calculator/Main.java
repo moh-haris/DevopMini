6f95680106bf11d6f7365ac330c50374303bca31
Mohammed Haris <113665185+moh-haris@users.noreply.github.com>
2026-03-06 12:50:38 +0530

    Add welcome message to calculator program

--- src/main/java/com/calculator/Main.java
+++ src/main/java/com/calculator/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Welcome to my calc prog ");
 
         while (true) {
             System.out.println("\nScientific Calculator");
